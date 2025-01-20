9557919cdb6b72a2de2d53ab1a614bb1435fb747
Moskize91 <i@taozeyu.com>
2025-01-20 12:40:42 +0800

    feat(blocks): support conversion logic (#2)

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # coqui-tts
 
-TTS is a library for advanced Text-to-Speech generation. This is a wrapper for coaui TTS.
+TTS is a library for advanced Text-to-Speech generation. This is a wrapper for [coqui TTS](https://github.com/coqui-ai/TTS).
--- tasks/conversion/__init__.py
+++ tasks/conversion/__init__.py
@@ -1,15 +1,12 @@
 import os
-import torch
 
 from oocana import Context
 from TTS.api import TTS
-from typing import cast
 
 def main(params: dict, context: Context):
-  text: str = params["text"]
   tts: TTS = params["model"]
-  speaker_path: str = params["speaker"]
-  language: str | None = params["language"]
+  source_path: str = params["source_speaker"]
+  target_path: str = params["target_speaker"]
   output_path: str | None = params["output_path"]
 
   if output_path is None:
@@ -17,10 +14,10 @@ def main(params: dict, context: Context):
       context.session_dir, 
       f"{context.job_id}.wav",
     )
-  tts.tts_to_file(
-    text=text, 
-    speaker_wav=speaker_path, 
-    language=cast(str, language), 
+
+  tts.voice_conversion_to_file(
+    source_wav=source_path,
+    target_wav=target_path,
     file_path=output_path,
   )
   return { "output_path": output_path }
